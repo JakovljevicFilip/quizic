8c802cc665026455652ddbebc34a932929c01937
filipJaci <filip.jakovljevic@hotmail.com>
2019-07-04 11:18:46 +0200

    Database Answers are now being shuffled on seeding

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -13,30 +13,39 @@ class AnswersTableSeeder extends Seeder
      */
     public function run()
     {
+        // GET ALL QUESTION IDS
         $questions = Question::all()->pluck('id');
+        // LOOP THROUGH QUESTION IDS
         $questions->each(function($item){
-            DB::table('answers')->insert([
-                [
-        			'text'=>'Correct answer',
-        			'status'=>true,
-        			'question_id'=>$item
-        		],
-        		[
-        			'text'=>'Incorrect answer',
-        			'status'=>false,
-        			'question_id'=>$item
-        		],
-        		[
-        			'text'=>'Incorrect answer',
-        			'status'=>false,
-        			'question_id'=>$item
-        		],
-        		[
-        			'text'=>'Incorrect answer',
-        			'status'=>false,
-        			'question_id'=>$item
-        		],
-        	]);
+            // CORRECT ANSWER INDEX
+            $correctAnswer = rand(1,4);
+            // ANSWERS ARRAY
+            $answers = [];
+
+            // LOOP 4 TIMES, THE NUMBER OF ANSWERS
+            for($i = 1; $i <= 4; $i++){
+                // INDEX OF CORRECT ANSWER
+                if($i === $correctAnswer){
+                    // APPEND CORRECT ANSWER TO ANSWERS ARRAY
+                    $answers[] = [
+                        'text'=>'Correct answer',
+                        'status'=>true,
+                        'question_id'=>$item
+                    ];
+                }
+                // OTHER INDEX
+                else{
+                    // APPEND INCORRECT ANSWER TO ANSWERS ARRAY
+                    $answers[] = [
+                        'text'=>'Incorrect answer',
+                        'status'=>false,
+                        'question_id'=>$item
+                    ];
+                }
+            }
+
+            // INSERT ANSWERS TO DB
+            DB::table('answers')->insert($answers);
         });
     }
 }
