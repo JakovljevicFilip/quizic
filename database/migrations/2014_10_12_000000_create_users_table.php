4a9b110678cfa4aeb1a811841b7deb0117ed58a1
filipJaci <filip.jakovljevic@hotmail.com>
2019-08-11 15:24:38 +0200

    Database - User migration role integer

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             // DEFAULT ROLE FOR REGULAR USERS
-            $table->int('role')->default(1);
+            $table->integer('role')->default(1);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
