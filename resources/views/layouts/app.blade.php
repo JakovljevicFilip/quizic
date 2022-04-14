5f625b67adcd9e14eafd3b12640dddeba01e921f
filipJaci <filip.jakovljevic@hotmail.com>
2022-04-14 10:27:24 +0200

    Tests script and style import on laravel view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,14 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://thequizic.herokuapp.com/js/app.js" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://thequizic.herokuapp.com/js/app.css" rel="stylesheet">
 </head>
 <body>
     @yield('content')
