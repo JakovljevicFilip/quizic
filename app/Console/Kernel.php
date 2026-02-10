2adf4d2fd9f8ae0931c521ee702e70314770a33e
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2026-02-10 13:18:48 +0100

    Makes job daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
         })->hourly();
 
         $schedule->command('demo:reseed')
-            ->everyMinute()
+            ->daily()
             ->onSuccess(function () {
                 \Log::info('demo:reseed completed');
             })
